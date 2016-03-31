331216c2718d0120d43579da145374d7b899ae17
dmitry <dimaaasik.s@gmail.com>
2016-03-31 23:18:26 +0600

    Changed file parts storing. Some handlers distributed in different actors.

--- src/main/java/storage/ContentActor.java
+++ src/main/java/storage/ContentActor.java
@@ -3,6 +3,7 @@ package storage;
 import info.smart_tools.smartactors.core.*;
 import info.smart_tools.smartactors.core.actors.Actor;
 import info.smart_tools.smartactors.core.actors.annotations.Handler;
+import info.smart_tools.smartactors.core.actors.db_accessor.DBFields;
 import info.smart_tools.smartactors.core.impl.SMObject;
 import info.smart_tools.smartactors.utils.ioc.IOC;
 import storage.util.FileInfoFields;
@@ -11,6 +12,7 @@ import java.util.LinkedList;
 import java.util.List;
 
 /**
+ * Формирует информацию о содержимом директории
  */
 public class ContentActor extends Actor {
 
@@ -18,21 +20,11 @@ public class ContentActor extends Actor {
     private ListField<IObject> fldrContentF;
     /** Формирование запросов в БД */
     private String fileInfoCollectionName;
-    private Field<String> collectionNameF;
-    private Field<Integer> pageSizeF;
-    private Field<Integer> pageNumberF;
-    private Field<IObject> queryF;
-    private ListField<IObject> searchResultF;
 
     public ContentActor(IObject params) {
 
         fldrPathF = new Field<>(new FieldName("folderPath"));
         fldrContentF = new ListField<>(new FieldName("folderContent"));
-        collectionNameF = new Field<>(new FieldName("collectionName"));
-        pageSizeF = new Field<>(new FieldName("pageSize"));
-        pageNumberF = new Field<>(new FieldName("pageNumber"));
-        queryF = new Field<>(new FieldName("query"));
-        searchResultF = new ListField<>(new FieldName("searchResult"));
         try {
             fileInfoCollectionName = new Field<String>(new FieldName("fileInfoCollectionName")).from(params, String.class);
         } catch (ChangeValueException | ReadValueException e) {
@@ -42,25 +34,24 @@ public class ContentActor extends Actor {
         }
     }
 
-    //получить запрос от клиента, тут же сформировать запрос на все файлы с path из message
     @Handler("formFldrContentQuery")
     public void formFldrContentQuery(IMessage msg) throws ChangeValueException, ReadValueException {
 
-        collectionNameF.inject(msg, fileInfoCollectionName);
-        pageSizeF.inject(msg, 1000000);
-        pageNumberF.inject(msg, 1);
+        DBFields.COLLECTION_NAME_FIELD.inject(msg, fileInfoCollectionName);
+        DBFields.PAGE_SIZE_FIELD.inject(msg, 1000000);
+        DBFields.PAGE_NUMBER_FIELD.inject(msg, 1);
         IObject query = new SMObject();
         IObject condition = new SMObject();
         condition.setValue(new FieldName("$eq"), fldrPathF.from(msg, String.class));
         query.setValue(new FieldName("logicPath"), condition);
-        queryF.inject(msg, query);
+        DBFields.QUERY_FIELD.inject(msg, query);
     }
 
     @Handler("sendFldrContent")
     public void sendFldrContent(IMessage msg) throws ReadValueException, ChangeValueException {
 
         List<IObject> fldrContentForSend = new LinkedList<>();
-        List<IObject> fldrContent = searchResultF.from(msg, IObject.class);
+        List<IObject> fldrContent = DBFields.SEARCH_RESULT_FIELD.from(msg, IObject.class);
         for(IObject fileInfo : fldrContent) {
             if(FileInfoFields.ACTIVE.from(fileInfo, Boolean.class)) {
                 IObject file = IOC.resolve(IObject.class);
--- src/main/java/storage/util/FileInfoFields.java
+++ src/main/java/storage/util/FileInfoFields.java
@@ -2,6 +2,7 @@ package storage.util;
 
 import info.smart_tools.smartactors.core.Field;
 import info.smart_tools.smartactors.core.FieldName;
+import info.smart_tools.smartactors.core.IObject;
 import info.smart_tools.smartactors.core.ListField;
 
 /**
@@ -9,6 +10,8 @@ import info.smart_tools.smartactors.core.ListField;
  */
 public class FileInfoFields {
 
+    public static final Field<IObject> FILE_INFO = new Field<>(new FieldName("fileInfo"));
+    public static final Field<IObject> PART_INFO = new Field<>(new FieldName("partInfo"));
     public static final Field<Integer> FILE_SIZE = new Field<>(new FieldName("fileSize"));
     public static final Field<Integer> PARTS_QUANTITY = new Field<>(new FieldName("partsQuantity"));
     public static final Field<Integer> PART_SIZE = new Field<>(new FieldName("partSize"));
@@ -17,7 +20,8 @@ public class FileInfoFields {
     public static final Field<String> LOGIC_PATH = new Field<>(new FieldName("logicPath"));
     public static final Field<String> PHYSIC_PATH = new Field<>(new FieldName("phyPath"));
     public static final Field<String> ORIGINAL_NAME = new Field<>(new FieldName("originalName"));
-    public static final ListField<Integer> PARTS_NUMBERS = new ListField<>(new FieldName("partsNumbers"));
-    public static final ListField<String> FILE_PARTS = new ListField<>(new FieldName("fileParts"));
+    public static final Field<String> PART = new Field<>(new FieldName("part"));
+    public static final Field<Integer> PART_NUMBER = new Field<>(new FieldName("partNumber"));
+
 
 }
